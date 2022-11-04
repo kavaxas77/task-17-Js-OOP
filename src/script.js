6a07b74baec7944530e21b032788837bf065cb40
kavaxas77 <112971973+kavaxas77@users.noreply.github.com>
2022-11-04 19:46:57 +0400

    task 17 js OOP

---
+++ src/script.js
@@ -0,0 +1,26 @@
+function Car(cName , cModel , cYear) {
+    this.name = cName
+    this.model = cModel
+    this.yaer = cYear
+    this.getcarInfo = function() {
+        document.write("model" + this.model + "gamoshvebis weli" + this.year)
+        document.write ("saxeli" + this.cname + "modeli" + this.cage + "weli" + this.cyear ) tu ese
+    }
+}
+
+function Person (pName , pAge) {
+    this.name = pName
+    this.age = pAge
+    this.car = function(car) {
+        document.write (" " + has + " " + car.model + " " + car.name + " " + car.year)
+    }
+    this.displayinfo = function() {
+        document.write (this.age + " " + year + " " + this.name)
+    }
+}
+
+var bill = new Person ("tom",24 და ყავს მანქანა Tesla Model S released in 2015)
+ Tom.displayinfo()
+
+ var bill = new Person ("bill",26 არ ყავს მანქანა)
+ bill.displayinfo()
